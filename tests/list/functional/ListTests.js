48eed34a1fd4c51eae5807e58faaa0af926307bc
Sebastien Brunot <sbrunot@gmail.com>
2014-01-24 14:01:26 +0100

    Added a few basic functional tests for the list.

--- tests/list/functional/ListTests.js
+++ tests/list/functional/ListTests.js
@@ -2,33 +2,52 @@ define(["intern!object",
         "intern/chai!assert",
         "require"
         ], function (registerSuite, assert, require) {
-	
-	console.log("# Registering list/List tests");
-	registerSuite({
-		name: "StarRating tests",
-		"Programmatic: Default store, items added before adding the list to the document and before startup": function () {
-			var remote = this.remote;
-			var listId = "list-prog-1";
+
+	var basicTest = function (remote, testPage, listId, numberOfItemsExpected, numberOfCategoriesExpected) {
+		return remote
+		.get(require.toUrl(testPage))
+		.waitForCondition("ready", 5000)
+		.then(function () {
 			return remote
-			.get(require.toUrl("./ListGallery.html"))
-			.waitForCondition("ready", 5000)
-			.then(function () {
-				remote
-				.elementById(listId)
-					.elementsByTagName("d-list-item")
-					.then( function (result) {
-						assert.equal(result.length, 100, "number of list items is not the expected one");
+			.elementById(listId)
+				.elementsByTagName("d-list-item")
+					.then(function (result) {
+						assert.equal(result.length, numberOfItemsExpected,
+								listId + " number of list items is not the expected one");
 						// TODO: check the label on each item
 					})
-					.end()
-					.elementsByTagName("d-category-item")
-					.then( function(result) {
-						assert.equal(result.length, 0, "number of category headers is not the expected one");
+				.elementsByTagName("d-list-category")
+					.then(function (result) {
+						assert.equal(result.length, numberOfCategoriesExpected,
+								listId + " number of category headers is not the expected one");
 					})
-					.end()
 					// TODO: scroll ?
 				.end();
-			})
+		});
+	};
+
+	registerSuite({
+		name: "StarRating tests",
+		"ListGallery.html / list-prog-1": function () {
+			return basicTest(this.remote, "./ListGallery.html", "list-prog-1", 100, 0);
 		},
+		"ListGallery.html / list-prog-2": function () {
+			return basicTest(this.remote, "./ListGallery.html", "list-prog-2", 100, 0);
+		},
+		"ListGallery.html / list-prog-3": function () {
+			return basicTest(this.remote, "./ListGallery.html", "list-prog-3", 100, 0);
+		},
+		"ListGallery.html / list-prog-4": function () {
+			return basicTest(this.remote, "./ListGallery.html", "list-prog-4", 100, 0);
+		},
+		"ListGallery.html / list-mark-1": function () {
+			return basicTest(this.remote, "./ListGallery.html", "list-mark-1", 10, 0);
+		},
+		"ListGallery.html / list-mark-2": function () {
+			return basicTest(this.remote, "./ListGallery.html", "list-mark-2", 10, 0);
+		},
+		"ListGallery.html / list-mark-3": function () {
+			return basicTest(this.remote, "./ListGallery.html", "list-mark-3", 10, 2);
+		}
 	});
 });
